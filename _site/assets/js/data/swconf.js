7d0be9d8366bc84ffd57dbe7e9863b7a70e15c43
Spect3r <spect3r@kali.Kali>
2025-06-29 03:34:13 +0300

    updating

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751114299',resources: [
+    cacheName: 'chirpy-1751156678',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
